8ec617281b6d929daa7fac35a7ef0716482b07ea
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-05-01 20:36:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreleftwrist=0;
+scorerightwrist=0;
 
 function preload(){
     song=loadSound("a.mp3");
@@ -23,8 +25,14 @@ function modelLoaded(){
 
 function gotPoses(results){
 if(results.length > 0){
-    console.results(results);
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("leftwistX="+leftWristX+" and leftwristY="+leftWristY);
+    scoreleftwrist=results[0].pose.keypoints[9].score;
+    console.log("scoreleftwrist="+scoreleftwrist);
 }
+
 }
 
 function play(){
@@ -35,6 +43,12 @@ function play(){
 
 function draw(){
     image(video,0,0,600,500);
-
-
+if(scoreleftwrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    CHEEze=Number(leftWristY);
+    URMom=floor(CHEEze);
+    volume=URMom/500;
+    document.getElementById("volume").innerHTML="volume="+volume;
+    song.setVolume(volume);
+}
 }
